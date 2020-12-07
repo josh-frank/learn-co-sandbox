7deb70500885480fd468785b135c2b5ea29ae955
Joshua Frank <eurosewusa@gmail.com>
2020-12-07 23:43:38 +0000

    Automatically backed up by Learn

---
+++ example.rb
@@ -0,0 +1,6 @@
+english_bands_by_city = {
+  :liverpool =>   "The Beatles",
+  :manchester =>  "The Smiths",
+  :coventry =>    "Della Derbyshire and the BBC Radiophonic Band"
+  :london => "Ziggy Stardust and the Spiders from Mars"
+}
